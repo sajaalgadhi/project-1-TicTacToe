eb8b832f68db70cbddb38b4f9af90a25ed90dcfd
sajaalgadhi <saja.algadhi@outlook.com>
2019-01-30 11:23:24 +0300

    last update

---
+++ main2.js
@@ -0,0 +1,127 @@
+//empty board 
+var gameBoard = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+]
+var turncheck = true;
+
+//function to get X and O on click and switch the choice for each turn 
+var turn = "X";
+function onClick(event) {
+
+    $(event.target).text(turn);
+    gameBoard[event.target.id] = turn;
+    $(event.target).css('font-size', '70px');
+    $(event.target).css('color', 'white');
+    $(event.target).css('font-family', 'courier');
+    $(event.target).off('click');
+    winnerCheck(); //switch between turns and print value of turn 
+    if (turn === 'X') {
+        $('#moves').text('O Turn');//show the next turn
+        if (turncheck === true) {            
+            for (var i = 0; i < 9; i++) {
+                if ($("#"+i).text() === '' && turncheck) {
+                    $("#"+i).text('O');
+                    $("#"+i).css('font-size', '70px');
+                    $("#"+i).css('color', 'white');
+                    $("#"+i).css('font-family', 'courier');
+                    $("#"+i).off('click');
+                    return;
+                }
+            }
+
+//will not allow user to click again in the same cell after game is begin
+        }
+
+    }
+
+}
+
+
+
+
+
+function computerturn() {
+    turncheck = true;
+}
+
+
+//chic winner
+function winnerCheck() {
+    // if top row win show message and play the song 
+    if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[0]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+    } else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+        // if middle row win  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[3]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+    } else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+        // if bottom row win  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[6]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+
+    } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+        // if left colum win  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[0]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+    } else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+        // if middle column win  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[1]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+    } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+        // if right diagbnol  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[2]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+
+    } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+        // if right diagnol  win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[2]).then(() => {
+            resetGame();//reset the board after winning
+        });
+
+    } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+        // if left diagnol win show message and play the song 
+        $("#WinSong").get(0).play();
+        swal('Winner is ' + gameBoard[0]).then(() => {
+            resetGame();//reset the board after winning
+        });//if No one win
+    } else if (gameBoard[0] !== '' && gameBoard[2] !== '' && gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' && gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '' && gameBoard[9] !== '') {
+        swal('No Winners , Try Again!').then(() => {
+            resetGame();//reset the board 
+        });
+    }
+}
+
+//Reset function to reset the game when the board is full or there is a winner
+function resetGame() {
+    location.reload();
+    $('.cells').text('');
+    $('#moves').text('');
+    $("#WinSong").get(0).pause();//pause song when the function end 
+}
+
+
+$('#reset').on('click', resetGame);
+$('.cells').on('click', onClick)
+// $('#start2').on('click', computerturn)
